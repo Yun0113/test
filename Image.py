468cf914a7de12f24d59837379ec5d27839e4194
Yun0113 <69342165+Yun0113@users.noreply.github.com>
2020-08-13 08:59:19 +0800

    Add files via upload

---
+++ Image.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#Author:Little Tang
+import numpy as np
+import cv2
+image = cv2.imread("C:/Users/Administrator/Desktop/test.jpg")
+cv2.imshow("Original",image)
+cv2.waitKey(0)
+#彩色转灰色
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray",gray)
+cv2.waitKey(0)
